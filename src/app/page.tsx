e1b8fa50ca46a1d51469a47521d3b33e93774430
MrMarxz <markus.marais@outlook.com>
2023-12-15 13:34:20 +0200

    Added card flip animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,42 @@
-import Image from 'next/image';
-import Button from 'react-bootstrap/Button';
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
 
 export default function HomePage() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
   return (
-    <main className="flex flex-col min-h-screen bg-green-900 text-white">
+    <main className="flex min-h-screen flex-col bg-green-900 text-white">
       <div className="flex flex-row justify-center">
         <h1 className="mt-3 text-5xl">Basic Strategy</h1>
       </div>
 
-      <div className="flex flex-col flex-grow">
-        <div className="flex-grow border border-white border-solid">
-          Card div
-          <div className='diagonal-move'>
-            <Image src="/svg_cards/2_of_clubs.svg" alt="Ace of Clubs" width={100} height={150} />
+      <div className="flex flex-grow flex-col">
+        <div className="flex-grow border border-solid border-white">
+          <div className="card-container mt-3" onClick={handleFlip}>
+            <div className={`card ${isFlipped ? "flipped" : ""}`}>
+              <div className="card-front">
+                <Image
+                  src="/card_back.svg"
+                  alt="Ace of Clubs"
+                  width={100}
+                  height={150}
+                />
+              </div>
+              <div className="card-back">
+                <Image src="/svg_cards/2_of_clubs.svg" alt="Ace of Clubs" width={100} height={150} />
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className="flex justify-end mt-2">
-          <div className="border border-white border-solid grid grid-cols-2 gap-4 w-1/3 p-3">
+        <div className="mt-2 flex justify-end">
+          <div className="grid w-1/3 grid-cols-2 gap-4 border border-solid border-white p-3">
             <Button variant="primary">HIT</Button>
             <Button variant="primary">STAND</Button>
             <Button variant="primary">DOUBLE</Button>
