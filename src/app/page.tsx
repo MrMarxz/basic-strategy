024a7c06fa2ed6f4c32f920c215e7feffd4f621b
MrMarxz <markus.marais@outlook.com>
2023-12-19 13:32:51 +0200

    Updated styling for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,29 +14,41 @@ export default function HomePage() {
 
   const [handleDealCount, setHandleDealCount] = useState(0);
 
-  const [playerFirstCardImagePath, setPlayerFirstCardImagePath] = useState(front);
-  const [playerSecondCardImagePath, setPlayerSecondCardImagePath] = useState(front);
-  const [dealerFirstCardImagePath, setDealerFirstCardImagePath] = useState(front);
+  const [playerFirstCardImagePath, setPlayerFirstCardImagePath] =
+    useState(front);
+  const [playerSecondCardImagePath, setPlayerSecondCardImagePath] =
+    useState(front);
+  const [dealerFirstCardImagePath, setDealerFirstCardImagePath] =
+    useState(front);
 
-  const [isDealingPlayerFirstCard, setIsDealingPlayerFirstCard] = useState(false);
-  const [isDealingPlayerSecondCard, setIsDealingPlayerSecondCard] = useState(false);
-  const [isDealingDealerFirstCard, setIsDealingDealerFirstCard] = useState(false);
-  const [isDealingDealerSecondCard, setIsDealingDealerSecondCard] = useState(false);
+  const [isDealingPlayerFirstCard, setIsDealingPlayerFirstCard] =
+    useState(false);
+  const [isDealingPlayerSecondCard, setIsDealingPlayerSecondCard] =
+    useState(false);
+  const [isDealingDealerFirstCard, setIsDealingDealerFirstCard] =
+    useState(false);
+  const [isDealingDealerSecondCard, setIsDealingDealerSecondCard] =
+    useState(false);
 
-  const [isPlayerFirstCardFlipped, setIsPlayerFirstCardFlipped] = useState(false);
-  const [isPlayerSecondCardFlipped, setIsPlayerSecondCardFlipped] = useState(false);
-  const [isDealerFirstCardFlipped, setIsDealerFirstCardFlipped] = useState(false);
-  const [isDealerSecondCardFlipped, setIsDealerSecondCardFlipped] = useState(false);
+  const [isPlayerFirstCardFlipped, setIsPlayerFirstCardFlipped] =
+    useState(false);
+  const [isPlayerSecondCardFlipped, setIsPlayerSecondCardFlipped] =
+    useState(false);
+  const [isDealerFirstCardFlipped, setIsDealerFirstCardFlipped] =
+    useState(false);
+  const [isDealerSecondCardFlipped, setIsDealerSecondCardFlipped] =
+    useState(false);
 
   const [isPlayerFirstCardHidden, setIsPlayerFirstCardHidden] = useState(true);
-  const [isPlayerSecondCardHidden, setIsPlayerSecondCardHidden] = useState(true);
+  const [isPlayerSecondCardHidden, setIsPlayerSecondCardHidden] =
+    useState(true);
   const [isDealerFirstCardHidden, setIsDealerFirstCardHidden] = useState(true);
-  const [isDealerSecondCardHidden, setIsDealerSecondCardHidden] = useState(true);
+  const [isDealerSecondCardHidden, setIsDealerSecondCardHidden] =
+    useState(true);
 
-  const [correctAction, setCorrectAction] = useState('');
+  const [correctAction, setCorrectAction] = useState("");
 
   const [newRoundStarted, setNewRoundStarted] = useState(0);
-
 
   useEffect(() => {
     // Generate random cards for the player and dealer
@@ -49,13 +61,16 @@ export default function HomePage() {
     setPlayerSecondCardImagePath(playerSecondCardData.imagePath);
     setDealerFirstCardImagePath(dealerFirstCardData.imagePath);
 
-    console.log('=====================');
-    console.log('player first card value', playerFirstCardData.cardValue);
-    console.log('player second card value', playerSecondCardData.cardValue);
-    console.log('dealer first card value', dealerFirstCardData.cardValue);
+    console.log("=====================");
+    console.log("player first card value", playerFirstCardData.cardValue);
+    console.log("player second card value", playerSecondCardData.cardValue);
+    console.log("dealer first card value", dealerFirstCardData.cardValue);
 
     // Get the correct action for the player
-    const correctAction: string = getCorrectAction(dealerFirstCardData.cardValue, [playerFirstCardData.cardValue, playerSecondCardData.cardValue]);
+    const correctAction: string = getCorrectAction(
+      dealerFirstCardData.cardValue,
+      [playerFirstCardData.cardValue, playerSecondCardData.cardValue],
+    );
     setCorrectAction(correctAction);
 
     // If the handleDeal is run, set all cards to be face down
@@ -101,7 +116,6 @@ export default function HomePage() {
   };
 
   const handleDeal = async () => {
-
     // Increment the handleDealCount
     setHandleDealCount(handleDealCount + 1);
 
@@ -140,44 +154,56 @@ export default function HomePage() {
   };
 
   const checkSelectedAction = async (selectedAction: string) => {
-
     if (selectedAction === correctAction) {
       toast.success("Correct!", {
-        duration: 2500,
+        duration: 2000,
       });
     } else {
       toast.error(`The correct action was: ${correctAction}`, {
-        duration: 2500,
+        duration: 2000,
       });
     }
 
     setTimeout(() => {
-      handleDeal().then(() => {
-        console.log('New round started');
-      }).catch((error) => {
-        console.log(error);
-      });
-    }, 2500);
+      handleDeal()
+        .then(() => {
+          console.log("New round started");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 2000);
   };
 
   return (
     <main className="flex min-h-screen flex-col bg-green-900 text-white">
-      <div className="flex flex-col">
+      <div className="flex flex-col items-center">
         <div className="flex flex-row justify-center">
           <h1 className="mt-3 text-5xl">Basic Strategy</h1>
         </div>
 
         <div
           style={{ height: 500 }}
-          className="flex h-fit flex-col border border-solid border-white"
+          className="mt-2 flex h-fit w-3/4 flex-col border border-solid border-white"
         >
           {/* Dealer Cards */}
           <div style={{ height: 150 }} className="flex flex-row justify-center">
-            <div className={ `${isDealingDealerFirstCard ? "move-dealer-first-card" : "blank"} ${isDealerFirstCardHidden ? "hidden" : "visible"}`  }>
+            <div
+              className={`${
+                isDealingDealerFirstCard ? "move-dealer-first-card" : "blank"
+              } ${isDealerFirstCardHidden ? "hidden" : "visible"}`}
+            >
               <Card front={front} isFlipped={isDealerFirstCardFlipped} />
             </div>
-            <div className={ `${isDealingDealerSecondCard ? "move-dealer-second-card" : "blank"} ${isDealerSecondCardHidden ? "hidden" : "visible" }` }>
-              <Card front={dealerFirstCardImagePath} isFlipped={isDealerSecondCardFlipped} />
+            <div
+              className={`${
+                isDealingDealerSecondCard ? "move-dealer-second-card" : "blank"
+              } ${isDealerSecondCardHidden ? "hidden" : "visible"}`}
+            >
+              <Card
+                front={dealerFirstCardImagePath}
+                isFlipped={isDealerSecondCardFlipped}
+              />
             </div>
           </div>
 
@@ -193,36 +219,66 @@ export default function HomePage() {
           {/* Player Cards */}
           <div style={{ height: 150 }} className="flex flex-row justify-center">
             <div
-              className={
-                `${isDealingPlayerFirstCard ? "move-player-first-card" : "blank"} ${isPlayerFirstCardHidden ? "hidden" : "visible"}`
-              }
+              className={`${
+                isDealingPlayerFirstCard ? "move-player-first-card" : "blank"
+              } ${isPlayerFirstCardHidden ? "hidden" : "visible"}`}
             >
-              <Card front={playerFirstCardImagePath} isFlipped={isPlayerFirstCardFlipped} />
+              <Card
+                front={playerFirstCardImagePath}
+                isFlipped={isPlayerFirstCardFlipped}
+              />
             </div>
             <div
-              className={
-                `${isDealingPlayerSecondCard ? "move-player-second-card" : "blank"} ${isPlayerSecondCardHidden ? "hidden" : "visible"}`
-              }
+              className={`${
+                isDealingPlayerSecondCard ? "move-player-second-card" : "blank"
+              } ${isPlayerSecondCardHidden ? "hidden" : "visible"}`}
             >
-              <Card front={playerSecondCardImagePath} isFlipped={isPlayerSecondCardFlipped} />
+              <Card
+                front={playerSecondCardImagePath}
+                isFlipped={isPlayerSecondCardFlipped}
+              />
             </div>
           </div>
         </div>
-
-        <div className="flex flex-col">
-          <div className="mt-2 flex justify-end">
-            <div className="grid w-1/3 grid-cols-2 gap-4 border border-solid border-white p-3">
-            <Button variant="primary" onClick={() => checkSelectedAction("hit")}>HIT</Button>
-            <Button variant="primary" onClick={() => checkSelectedAction("stand")}>STAND</Button>
-            <Button variant="primary" onClick={() => checkSelectedAction("double")}>DOUBLE</Button>
-            <Button variant="primary" onClick={() => checkSelectedAction("split")}>SPLIT</Button>
-            <Button variant="primary" onClick={() => checkSelectedAction("surrender")}>SURRENDER</Button>
-            <Button variant="primary" onClick={() => checkSelectedAction("insurance")}>INSURANCE</Button>
-              <Button variant="primary" onClick={handleDeal}>
-                DEAL
-              </Button>
-            </div>
-          </div>
+      </div>
+      <div className="mt-5 flex flex-row justify-center">
+        <div className="grid grid-cols-2 gap-4 border border-solid border-white p-3">
+          <Button variant="primary" onClick={() => checkSelectedAction("hit")}>
+            HIT
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => checkSelectedAction("stand")}
+          >
+            STAND
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => checkSelectedAction("double")}
+          >
+            DOUBLE
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => checkSelectedAction("split")}
+          >
+            SPLIT
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => checkSelectedAction("surrender")}
+          >
+            SURRENDER
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => checkSelectedAction("insurance")}
+          >
+            INSURANCE
+          </Button>
+          <Button variant="primary" onClick={handleDeal}>
+            DEAL
+          </Button>
         </div>
       </div>
     </main>
